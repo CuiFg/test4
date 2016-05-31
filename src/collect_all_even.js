a1cd1f4e74dbc1a4dd5c2524dcf3615103f23929
CuiFg <1174910857@qq.com>
2016-05-31 23:33:00 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,22 @@
 
-function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+function collect_same_elements(collection_a, object_b) 
+{
+
+ var arr = [];//先定义一个空的数组
+	var same = 0;
+	for(var i=0; i<collection_a.length; i++)
+	{
+	   same = collection_a[i].key;
+	   	for(var j=0; j<object_b.value.length; j++)
+		{
+			     if(object_b.value[j] == same)
+				 {
+				    arr.push(same);
+			     }
+		}
+	}	
+return arr;
 }
+
 
 module.exports = collect_same_elements;
